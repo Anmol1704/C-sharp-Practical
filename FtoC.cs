491db90406ea7818f3f4f3bded2fbf11d1b272fc
Anmol1704 <156204564+Anmol1704@users.noreply.github.com>
2024-11-30 21:22:33 +0530

    Add files via upload

---
+++ FtoC.cs
@@ -0,0 +1,9 @@
+class FtoC
+{
+	public static void Main(string[] args)
+	{
+		float f = System.Single.Parse(args [0]);
+		double C = System.Convert.ToDouble((f - 32)/1.8);
+		System.Console.WriteLine("C = "+C);
+	}
+}
